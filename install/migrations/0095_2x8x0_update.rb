6b7bf5dfc7db2aad455b5d6a2f44156b8a612a36
lynnfaraday <lynnfaraday@users.noreply.github.com>
2025-10-02 23:49:28 -0400

    Locations legend.

--- install/migrations/0095_2x8x0_update.rb
+++ install/migrations/0095_2x8x0_update.rb
@@ -11,6 +11,11 @@ module AresMUSH
         config = DatabaseMigrator.read_config_file("sites.yml")
         config["sites"]["max_connections"] = 20
         DatabaseMigrator.write_config_file("sites.yml", config)
+        
+        Global.logger.debug "Adding location legend"
+        config = DatabaseMigrator.read_config_file("rooms.yml")
+        config["rooms"]["icon_legend"] = {}
+        DatabaseMigrator.write_config_file("rooms.yml", config)        
       end
     end
   end    
